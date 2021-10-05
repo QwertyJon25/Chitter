7c35c0bacc117ab2e9fc3321df18551ff81e4d92
Alex Aguirre <aguirre_alex@rocketmail.com>
2021-10-05 18:50:20 -0500

    NavBar update

--- chitter-project/src/Components/App.jsx
+++ chitter-project/src/Components/App.jsx
@@ -1,19 +1,53 @@
-import React from "react";
+import React, {useState, useEffect} from "react"
 import './App.css';
 import Main from "../Main.jsx"
 import NavMenu from ">/NavMenu.jsx";
 import UserList from "../UserList.jsx";
-import {}
 
 function App() {
+  //State Variables for JSON data
+  const [users, setUsers] = useState([]);
+  const [posts, setPosts] = useState([]);
+
+  //State Variable for Current User
+  const [currentUser, setCurrentUser] = useState({})
+
+  //State Variable for Form data
+  const [formData, setFormData] = useState({
+    username:"",
+    profilePic:"",
+    bio:""
+  });
 
 
+  useEffect(() => {
+    fetch("")
+    .then((res) => res.json())
+    .then((userData) => setPoems(userData));
+
+    fetch("")
+    .then((res) => res.json())
+    .then((postData) => setPoems(postData));
+  }, [])
 
   return (
     <div className="App">
-    
+    <NavMenu currentUser={currentUser} setFormData={setFormData}/>
     </div>
   );
 }
 
 export default App;
+
+
+// function addPoem(newPoem){
+//   fetch("http://localhost:8004/poems", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(newPoem)
+//   })
+//   .then((res) => res.json())
+//   .then((newPoemData) => setPoems([...poems, newPoemData]))
+// }
--- chitter-project/src/Components/NavMenu.jsx
+++ chitter-project/src/Components/NavMenu.jsx
@@ -1,12 +1,40 @@
 import React from 'react'
 
-export default function NavMenu() {
+export default function NavMenu({currentUser, setFormData}) {
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log(formData);
+      }
+      
+      function handleChange(event) {
+        const key = event.target.id
+        const value = event.target.value
+        
+        setFormData({ 
+          ...formData, 
+          [key]: value
+        })
+      }
 
-
-    
     return (
         <div>
-            
+            <button>Home</button>
+            <form onSubmit={handleSubmit}>
+                <h2>Sign Up!</h2>
+                <input id="username" type="text" placeholder="Enter username" onChange={handleChange}/>
+                <input id="profilePic" type="text" placeholder="Enter a profile picture" onChange={handleChange}/>
+                <textarea id="bio" type="text" placeholder="Enter a bio" rows={3} onChange={handleChange}/>
+                <input type="submit" value="Sign Up" />
+            </form>
+            <button>Hide Form</button>
+            <img src="" alt="user image"></img>
+            <button>Sign Out</button>
         </div>
     )
 }
+
+// Sign up form
+// Hide/show form button
+// Home button
+// Account button
+// Sign-out Button
