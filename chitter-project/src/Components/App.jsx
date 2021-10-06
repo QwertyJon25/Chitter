01442b6def08a25b05b6bb67580a83c8c619e2a6
Jon Courtier <jonaqwerty@yahoo.com>
2021-10-06 09:00:56 -0400

    Server dependency addition

--- chitter-project/src/Components/App.jsx
+++ chitter-project/src/Components/App.jsx
@@ -1,38 +1,38 @@
 import React, {useState, useEffect} from "react"
-import './App.css';
-import Main from "../Main.jsx"
+// import './App.css';
+// import Main from "../Main.jsx"
 import NavMenu from ">/NavMenu.jsx";
-import UserList from "../UserList.jsx";
+// import UserList from "../UserList.jsx";
 
 function App() {
   //State Variables for JSON data
-  const [users, setUsers] = useState([]);
-  const [posts, setPosts] = useState([]);
+  // const [users, setUsers] = useState([]);
+  // const [posts, setPosts] = useState([]);
 
   //State Variable for Current User
-  const [currentUser, setCurrentUser] = useState({})
+  // const [currentUser, setCurrentUser] = useState({})
 
   //State Variable for Form data
-  const [formData, setFormData] = useState({
-    username:"",
-    profilePic:"",
-    bio:""
-  });
+  // const [formData, setFormData] = useState({
+  //   username:"",
+  //   profilePic:"",
+  //   bio:""
+  // });
 
 
-  useEffect(() => {
-    fetch("")
-    .then((res) => res.json())
-    .then((userData) => setPoems(userData));
+  // useEffect(() => {
+  //   fetch("http://localhost:4000/users")
+  //   .then((res) => res.json())
+  //   .then((userData) => setUsers(userData));
 
-    fetch("")
-    .then((res) => res.json())
-    .then((postData) => setPoems(postData));
-  }, [])
+  //   fetch("http://localhost:4000/posts")
+  //   .then((res) => res.json())
+  //   .then((postData) => setPosts(postData));
+  // }, [])
 
   return (
     <div className="App">
-    <NavMenu currentUser={currentUser} setFormData={setFormData}/>
+    <NavMenu currentUser={currentUser} setFormData={setFormData} formData={formData}/>
     </div>
   );
 }
--- chitter-project/src/Components/NavMenu.jsx
+++ chitter-project/src/Components/NavMenu.jsx
@@ -1,33 +1,33 @@
 import React from 'react'
 
-export default function NavMenu({currentUser, setFormData}) {
-    function handleSubmit(event) {
-        event.preventDefault();
-        console.log(formData);
-      }
+export default function NavMenu({ currentUser, setFormData, formData }) {
+
+    // function handleSubmit(event) {
+    //     event.preventDefault();
+    //   }
       
-      function handleChange(event) {
-        const key = event.target.id
-        const value = event.target.value
+    //   function handleChange(event) {
+    //     const key = event.target.id
+    //     const value = event.target.value
         
-        setFormData({ 
-          ...formData, 
-          [key]: value
-        })
-      }
+    //     setFormData({ 
+    //       ...formData, 
+    //       [key]: value
+    //     })
+    //   }
 
     return (
         <div>
             <button>Home</button>
-            <form onSubmit={handleSubmit}>
+            <form >
                 <h2>Sign Up!</h2>
-                <input id="username" type="text" placeholder="Enter username" onChange={handleChange}/>
-                <input id="profilePic" type="text" placeholder="Enter a profile picture" onChange={handleChange}/>
-                <textarea id="bio" type="text" placeholder="Enter a bio" rows={3} onChange={handleChange}/>
+                <input id="username" type="text" placeholder="Enter username" />
+                <input id="profilePic" type="text" placeholder="Enter a profile picture" />
+                <textarea id="bio" type="text" placeholder="Enter a bio" rows={3} />
                 <input type="submit" value="Sign Up" />
             </form>
             <button>Hide Form</button>
-            <img src="" alt="user image"></img>
+            {/* <img src="" alt="user image"></img> */}
             <button>Sign Out</button>
         </div>
     )
