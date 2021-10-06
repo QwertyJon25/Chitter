c07f8bb68bf1723f815126555b9439a3c7eae4dd
Jon Courtier <jonaqwerty@yahoo.com>
2021-10-06 09:49:50 -0400

    Card component syntax

--- chitter-project/src/Components/UserCard.jsx
+++ chitter-project/src/Components/UserCard.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 
-export default function UserCard() {
+export default function UserCard({ userData }) {
+    const { username, profilePic, bio } = userData
 
-
+//onClick Functionality to profile pic to redirect to "profile page" rendered in main component
     
     return (
-        <div>
-            
-        </div>
+       <li className="card">
+           <img src ={profilePic} alt={username} />
+           <h4>{username}</h4>
+           <h5>{bio}</h5>
+       </li>
     )
 }
