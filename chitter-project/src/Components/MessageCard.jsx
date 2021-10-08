90d544a377269fba14b4ac93af73a9716de696d6
Jon Courtier <jonaqwerty@yahoo.com>
2021-10-08 09:28:26 -0400

    additions

--- chitter-project/src/Components/MessageCard.jsx
+++ chitter-project/src/Components/MessageCard.jsx
@@ -5,14 +5,17 @@ export default function MessageCard({postData, user}) {
     const {content, likes} = postData
 
     const[likeCount, setLikeCount] = useState(likes);
+    const [decrement, setDecrement] = useState(likes);
     
     return (
         <div>
             <img src={profilePic} alt="pic"/><br/>
             <span>{username}</span>
             <p>{content}</p>
-            <button onClick={() => setLikeCount(likeCount + 1)}>❤</button>
-            <span>{likeCount}</span>
+            <button onClick={() => setLikeCount(likeCount + 1)}>❤️</button>
+            <span>{likeCount}</span> <button>Delete Post</button><br/>
+            <button onClick={() => setDecrement (decrement - 1)}>💔</button>
+            <span>{decrement}</span> 
         </div>
     )
 }
